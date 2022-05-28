4b9408549378e73fc8c7e7e7dee944454504c7dc
quarkusio <no-reply@quarkus.io>
2022-05-28 19:56:22 +0000

    Initial commit

---
+++ src/test/java/com/aantivero/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.aantivero;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
